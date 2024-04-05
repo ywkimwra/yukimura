3869b3b320ea1eaf46d4a1f2849f20550a5d6d43
ywkimwra <lchuy97@gmail.com>
2024-04-06 02:54:09 +0700

    keke

--- CODEFORCES/1692A Marathon.py
+++ CODEFORCES/1692A Marathon.py
@@ -1,16 +1,6 @@
 t = int(input())
 
 for i in range(t):
-    a, b, c, d = map(int, input().split())
-    count = 0
-    flag = True
-    while flag:
-        if b > a:
-            count += 1
-        elif c > a:
-            count += 1
-        elif d > a:
-            count += 1
-        else:
-            flag = False
-    print(count)
+    a, b, c, d = list([map(int, input().split())])
+
+    
