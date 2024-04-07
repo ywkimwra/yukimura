6fc35b2da4b2b91aa4b7e9d6bb82202012949af8
ywkimwra <lchuy97@gmail.com>
2024-04-07 07:31:37 +0700

    keke

--- CODEFORCES/1692A Marathon.py
+++ CODEFORCES/1692A Marathon.py
@@ -1,6 +1,13 @@
 t = int(input())
+test_cases = []
 
 for i in range(t):
-    a, b, c, d = list([map(int, input().split())])
+    a, b, c, d = list(map(int, input().split()))
+    test_cases.append([a, b, c, d])
 
-    
+for j in range(t):
+    count = 0
+    for k in range(1, 4):
+        if test_cases[j][k] > test_cases[j][0]:
+            count += 1
+    print(count)
---
+++ CODEFORCES/41A Translation.py
@@ -0,0 +1,7 @@
+word = input()
+reversed = input()
+
+if word[::-1] == reversed:
+    print("YES")
+else:
+    print("NO")
