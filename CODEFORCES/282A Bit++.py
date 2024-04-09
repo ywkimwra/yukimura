9981bc0c2126a23ebe937959f0bdf90920d01d9d
ywkimwra <lchuy97@gmail.com>
2024-04-09 18:42:46 +0700

    done

---
+++ CODEFORCES/282A Bit++.py
@@ -0,0 +1,18 @@
+#https://codeforces.com/problemset/problem/282/A
+
+n = int(input())
+commands = []
+result = 0
+
+for i in range(n):
+    temp = input()
+    commands.append(temp)
+
+for i in commands:
+    if i[:2] == "++" or i[1:] == "++":
+        result += 1
+    elif i[:2] == "--" or i[1:] == "--":
+        result -= 1
+    
+print(result)
+
