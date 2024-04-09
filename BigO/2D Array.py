6b614e5fa2cd2a2a7607cd380792e347c0a93dd6
ywkimwra <lchuy97@gmail.com>
2024-04-09 11:35:42 +0700

    done

--- BigO/2D Array.py
+++ BigO/2D Array.py
@@ -1,12 +1,33 @@
+#BT1
+
+# nrows, ncols = map(int, input().split())
+# a = []
+# sum = 0
+
+# for i in range(nrows):
+#     temp = list(map(int, input().split()))
+#     a.append(temp)
+
+# for i in range(nrows):
+#     sum = 0
+#     for j in range(ncols):
+#         sum += a[i][j]
+#     print(sum, end=" ")
+
+#BT2
+
 nrows, ncols = map(int, input().split())
 a = []
-sum = 0
 
 for i in range(nrows):
-    temp = map(int, input().split())
+    temp = list(map(int, input().split()))
     a.append(temp)
 
-for i in range(nrows):
-    for j in range(ncols):
-        print(a[i][j], end= " ")
-    print()
+for j in range(ncols):
+    all_negative = True
+    for i in range(nrows):
+        if a[i][j] >= 0:
+            all_negative = False
+            break
+    if all_negative:
+        print(j + 1)
