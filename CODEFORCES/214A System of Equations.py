9ead2f50bb0433f9b9ecfc3c5608dc819fd8a72d
ywkimwra <lchuy97@gmail.com>
2024-04-09 15:34:17 +0700

    done

--- CODEFORCES/214A System of Equations.py
+++ CODEFORCES/214A System of Equations.py
@@ -1,1 +1,9 @@
 n, m = map(int, input().split())
+count = 0
+
+for a in range(0, 1001):
+    for b in range(0, 1001):
+        if a**2 + b == n and a + b**2 == m:
+            count += 1
+
+print(count)
