01f386e554a18856f39d3a60bd74c6b5811779cf
ywkimwra <lchuy97@gmail.com>
2024-04-12 10:40:32 +0700

    done

---
+++ CODEFORCES/791A Bear and Big Brother.py
@@ -0,0 +1,11 @@
+#https://codeforces.com/problemset/problem/791/A
+
+a, b = map(int, input().split())
+year_counter = 0
+while a <= b:
+    a *= 3
+    b *= 2
+    year_counter += 1
+
+print(year_counter)
+
