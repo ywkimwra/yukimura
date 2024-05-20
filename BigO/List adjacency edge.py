da81727384ae969ce1c51e65a245ccec301b25f3
ywkimwra <lchuy97@gmail.com>
2024-05-21 00:05:24 +0700

    done

--- BigO/List adjacency edge.py
+++ BigO/List adjacency edge.py
@@ -6,9 +6,10 @@ class Edge:
 
     def __str__(self):
         return f"{self.u} {self.v}"
-    
+
     def __lt__(self, other):
         return self.w < other.w
+
 
 M, K = map(int, input().split())
 edges = []
@@ -20,5 +21,8 @@ for _ in range(M):
 
 edges.sort()
 
-for i in range(K):
-    print(edges[i][::-1])
+edges_update = edges[:K]
+edges_update = edges_update[::-1]
+
+for edge in edges_update:
+    print(edge)
